ee556c83ea9a694ed09229c3c57d6d9211be8e4c
chandu3015 <chandu3015@gmail.com>
2018-12-20 12:39:01 -0600

    added book,author repositories

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -17,10 +17,15 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
+
+    public Author() {
+    }
+
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
 
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
